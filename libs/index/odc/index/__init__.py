5eb8ccc338c1dee6ff99cea2add934a9611cf3b0
Kirill Kouzoubov <kirill888@gmail.com>
2019-08-19 11:25:42 +1000

    Expose bin_dataset_stream from odc.index

--- libs/index/odc/index/__init__.py
+++ libs/index/odc/index/__init__.py
@@ -4,6 +4,7 @@
 from . _index import (
     from_metadata_stream,
     from_yaml_doc_stream,
+    bin_dataset_stream,
     dataset_count,
     count_by_year,
     count_by_month,
@@ -24,6 +25,7 @@ from ._uuid import (
 __all__ = (
     "from_yaml_doc_stream",
     "from_metadata_stream",
+    "bin_dataset_stream",
     "dataset_count",
     "count_by_year",
     "count_by_month",
