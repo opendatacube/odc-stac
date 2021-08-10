5a93f62f48b406d5e52b1256b9af29a49c0d9dbc
Kirill Kouzoubov <kirill888@gmail.com>
2021-08-10 12:28:10 +1000

    Change version management strategy #299
    1. Use `setup.cfg` to configure setuptools instead of `setup.py`
    2. Add `__version__` property to every package
    3. Use manual version management instead of `setuptools_scm`
    4. Use github actions "run number" instead of git to increment patch
       component of the version number on every commit to develop branch
    5. Include long_description from README.md for every package
    6. Use `packages = find_namespace:` directive for automatic package finding

--- libs/index/setup.py
+++ libs/index/setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="odc_index",
-    use_scm_version={"root": "../..", "relative_to": __file__},
-    setup_requires=["setuptools_scm"],
-    author="Open Data Cube",
-    author_email="",
-    maintainer="Open Data Cube",
-    maintainer_email="",
-    description="Datacube index prototypes/tools",
-    long_description="",
-    license="Apache License 2.0",
-    tests_require=["pytest", "deepdiff"],
-    install_requires=["datacube", "jinja2", "odc_io", "toolz", "pyproj"],
-    packages=["odc.index"],
-    zip_safe=False,
-)
+setup()
