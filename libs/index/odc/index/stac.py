4288cd88b3a6c42f2666ef3f5d94e1376045ba03
Alex Leith <alexgleith@gmail.com>
2021-08-11 10:26:20 +1000

    Fix stac_transform to replace - with _

--- libs/index/odc/index/stac.py
+++ libs/index/odc/index/stac.py
@@ -40,6 +40,8 @@ def _stac_product_lookup(
     if product_name is None:
         # If there's no odc:product, platform and collection, then fail.
         product_name = get_in(["collection"], item, no_default=True)
+    # Product names can't have dashes in them, for some reason
+    product_name = product_name.replace("-", "_")
     region_code = get_in(["odc:region_code"], properties, None)
     default_grid = None
 
