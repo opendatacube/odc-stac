cd0b341076ee57172f7eb717ba03c8a714ebc8b8
Kirill Kouzoubov <kirill888@gmail.com>
2021-11-08 18:55:34 +1100

    Add notebook render action

--- notebooks/stac-load-S2-ms.py
+++ notebooks/stac-load-S2-ms.py
@@ -8,7 +8,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.13.0
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
