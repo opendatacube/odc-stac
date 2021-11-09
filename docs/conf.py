95473d98eabf363f7908773b2faaadf5670a591b
Kirill Kouzoubov <kirill888@gmail.com>
2021-11-09 20:31:19 +1100

    Fix styling issues when display xarrays

--- docs/conf.py
+++ docs/conf.py
@@ -144,3 +144,5 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = ["xr-fixes.css"]
