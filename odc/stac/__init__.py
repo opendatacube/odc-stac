45da8a535e01a06d0ec1ebf6ddc62a7f1148f9ac
Kirill Kouzoubov <kirill888@gmail.com>
2022-02-25 18:48:44 +1100

    Import top-level classes from correct sources

--- odc/stac/__init__.py
+++ odc/stac/__init__.py
@@ -1,19 +1,24 @@
 """STAC Item -> ODC Dataset[eo3]."""
 from ._version import __version__  # isort:skip  this has to be 1st import
 from ._dcload import configure_rio, dc_load
-from ._eo3converter import (
-    RasterBandMetadata,
-    ConversionConfig,
-    infer_dc_product,
-    stac2ds,
-)
+from ._eo3converter import infer_dc_product, stac2ds
 from ._load import eo3_geoboxes, load
+from ._mdtools import ConversionConfig
+from ._model import (
+    RasterBandMetadata,
+    RasterCollectionMetadata,
+    RasterLoadParams,
+    RasterSource,
+)
 
 stac_load = load
 
 
 __all__ = (
     "RasterBandMetadata",
+    "RasterCollectionMetadata",
+    "RasterLoadParams",
+    "RasterSource",
     "ConversionConfig",
     "stac2ds",
     "infer_dc_product",
