77ed9960674cd86584bf3df0a86789c2f2ff1225
Kirill Kouzoubov <kirill888@gmail.com>
2022-05-02 10:46:04 +1000

    Add mypy asserts to test fictures

--- odc/stac/testing/fixtures.py
+++ odc/stac/testing/fixtures.py
@@ -4,9 +4,9 @@ Test fixture construction utilities.
 from contextlib import contextmanager
 from typing import Generator
 
-import odc.geo.xr  # pylint: disable=unused-import
 import rasterio
 import xarray as xr
+from odc.geo.xr import ODCExtensionDa
 
 
 @contextmanager
@@ -14,6 +14,8 @@ def with_temp_tiff(data: xr.DataArray, **cog_opts) -> Generator[str, None, None]
     """
     Dump array to temp image and return path to it.
     """
+    assert isinstance(data.odc, ODCExtensionDa)
+
     with rasterio.MemoryFile() as mem:
         data.odc.write_cog(mem.name, **cog_opts)
         yield mem.name
