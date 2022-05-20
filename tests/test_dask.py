b6017b98b795508901ae7705a15509aee104472a
Kirill Kouzoubov <kirill888@gmail.com>
2022-05-20 19:04:20 +1000

    Helper utility for dask graph construction

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -1,7 +1,7 @@
 from dask.base import tokenize
 from odc.geo.geobox import CRS, GeoBox, GeoboxTiles
 
-from odc.stac import _dask as _
+from odc.stac._dask import tokenize_stream
 
 
 def test_tokenize_odc_geo():
@@ -21,3 +21,19 @@ def test_tokenize_odc_geo():
     assert tokenize(crs) == tokenize(CRS(crs))
     assert tokenize(CRS("epsg:4326")) == tokenize(CRS("EPSG:4326"))
     assert tokenize(CRS("epsg:4326")) != tokenize(CRS("EPSG:3857"))
+
+
+def test_tokenize_stream():
+    dd = [1, 2, "", 2]
+    kx = list(tokenize_stream(dd))
+    assert kx == [(tokenize(d), d) for d in dd]
+
+    kx = list(tokenize_stream(dd, lambda k: (k, "d")))
+    assert kx == [((tokenize(d), "d"), d) for d in dd]
+
+    dsk = {}
+    kx = list(tokenize_stream(dd, None, dsk))
+    assert len(dsk) == len(set(dd))
+
+    for k, x in kx:
+        assert dsk[k] is x
