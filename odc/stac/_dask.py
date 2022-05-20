4a763395b4c09d655d23630a35290265f4617ff3
Kirill Kouzoubov <kirill888@gmail.com>
2022-05-20 19:04:20 +1000

    Helper utility for dask graph construction

--- odc/stac/_dask.py
+++ odc/stac/_dask.py
@@ -1,9 +1,13 @@
 """
 Various Dask helpers.
 """
+from typing import Any, Callable, Hashable, Iterator, MutableMapping, Optional, Tuple
+
 import odc.geo.crs
 import odc.geo.geobox
-from dask.base import normalize_token
+from dask.base import normalize_token, tokenize
+
+from ._model import T
 
 
 # TODO: these classes should just implement __dask_token__ instead
@@ -29,3 +33,21 @@ def normalize_token_gbt(gbt: odc.geo.geobox.GeoboxTiles):
         *gbox.shape.yx,
         *gbox.affine[:6],
     )
+
+
+def tokenize_stream(
+    xx: Iterator[T],
+    key: Callable[[str], Hashable] = None,
+    dsk: Optional[MutableMapping[Hashable, Any]] = None,
+) -> Iterator[Tuple[Hashable, T]]:
+    if key:
+        kx = ((key(tokenize(x)), x) for x in xx)
+    else:
+        kx = ((tokenize(x), x) for x in xx)
+
+    if dsk is None:
+        yield from kx
+    else:
+        for k, x in kx:
+            dsk[k] = x
+            yield k, x
