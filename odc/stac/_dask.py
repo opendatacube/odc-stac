5227f06a2bd15fc09d77d1700982624a3b07d72d
Kirill Kouzoubov <kirill888@gmail.com>
2022-07-14 19:37:45 +1000

    Only register dask tokenizer when needed
    newer odc.geo declares those already

--- odc/stac/_dask.py
+++ odc/stac/_dask.py
@@ -9,30 +9,30 @@ from dask.base import normalize_token, tokenize
 
 from ._model import T
 
+# newer version of odc.geo implement __dask_token__
+# only register those if working with older odc.geo
+if getattr(odc.geo.crs.CRS, "__dask_tokenize__", None) is None:
 
-# TODO: these classes should just implement __dask_token__ instead
-@normalize_token.register(odc.geo.crs.CRS)
-def normalize_token_crs(crs):
-    return ("odc.geo.crs.CRS", str(crs))
+    @normalize_token.register(odc.geo.crs.CRS)
+    def normalize_token_crs(crs):
+        return ("odc.geo.crs.CRS", str(crs))
 
+    @normalize_token.register(odc.geo.geobox.GeoBox)
+    def normalize_token_geobox(gbox):
+        crs = gbox.crs
+        return ("odc.geo.geobox.GeoBox", str(crs), *gbox.shape.yx, *gbox.affine[:6])
 
-@normalize_token.register(odc.geo.geobox.GeoBox)
-def normalize_token_geobox(gbox):
-    crs = gbox.crs
-    return ("odc.geo.geobox.GeoBox", str(crs), *gbox.shape.yx, *gbox.affine[:6])
-
-
-@normalize_token.register(odc.geo.geobox.GeoboxTiles)
-def normalize_token_gbt(gbt: odc.geo.geobox.GeoboxTiles):
-    gbox = gbt.base
-    crs = gbox.crs
-    return (
-        "odc.geo.geobox.GeoboxTiles",
-        *gbt.shape.yx,
-        str(crs),
-        *gbox.shape.yx,
-        *gbox.affine[:6],
-    )
+    @normalize_token.register(odc.geo.geobox.GeoboxTiles)
+    def normalize_token_gbt(gbt: odc.geo.geobox.GeoboxTiles):
+        gbox = gbt.base
+        crs = gbox.crs
+        return (
+            "odc.geo.geobox.GeoboxTiles",
+            *gbt.shape.yx,
+            str(crs),
+            *gbox.shape.yx,
+            *gbox.affine[:6],
+        )
 
 
 def tokenize_stream(
