2cd45aeef0f65a76736b4f5cdb308b1ed1c5beb4
Kirill Kouzoubov <kirill888@gmail.com>
2024-02-09 15:27:47 +1100

    Splitting _load.py step 2

--- odc/stac/_stac_load.py
+++ odc/stac/_stac_load.py
@@ -11,13 +11,11 @@ from typing import (
     Any,
     Callable,
     Dict,
-    Hashable,
     Iterable,
     Iterator,
     List,
     Literal,
     Optional,
-    Protocol,
     Sequence,
     Tuple,
     Union,
@@ -28,17 +26,13 @@ import numpy as np
 import pystac
 import pystac.item
 import xarray as xr
-from dask import array as da
 from dask.array.core import normalize_chunks
-from dask.base import quote, tokenize
 from dask.utils import ndeepmap
 from numpy.typing import DTypeLike
 from odc.geo import CRS, MaybeCRS, SomeResolution
 from odc.geo.geobox import GeoBox, GeoboxAnchor, GeoboxTiles
 from odc.geo.types import Unset
-from odc.geo.xr import xr_coords
 
-from ._dask import unpack_chunks
 from ._mdtools import ConversionConfig, output_geobox, parse_items, with_default
 from ._model import (
     BandQuery,
@@ -46,11 +40,11 @@ from ._model import (
     RasterBandMetadata,
     RasterCollectionMetadata,
     RasterLoadParams,
-    RasterSource,
 )
-from ._reader import SomeReader, nodata_mask, resolve_src_nodata
+from ._reader import SomeReader
 from ._rio import RioReader
 from ._utils import SizedIterable, pmap
+from .loader import DaskGraphBuilder, LoadChunkTask, fill_2d_slice, mk_dataset
 
 DEFAULT_CHUNK_FOR_LOAD = 2048
 """Used to partition load when not using Dask."""
@@ -63,119 +57,6 @@ Groupby = Union[str, GroupbyCallback]
 def _reader_driver(cfg: RasterLoadParams) -> SomeReader:
     # pylint: disable=unused-argument
     return RioReader()
-
-
-class MkArray(Protocol):
-    """Internal interface."""
-
-    # pylint: disable=too-few-public-methods
-    def __call__(
-        self,
-        shape: Tuple[int, ...],
-        dtype: DTypeLike,
-        /,
-        name: Hashable,
-    ) -> Any: ...  # pragma: no cover
-
-
-@dataclasses.dataclass(frozen=True)
-class _LoadChunkTask:
-    band: str
-    srcs: List[Tuple[int, str]]
-    cfg: RasterLoadParams
-    gbt: GeoboxTiles
-    idx_tyx: Tuple[int, int, int]
-
-    @property
-    def dst_roi(self):
-        t, y, x = self.idx_tyx
-        return (t, *self.gbt.roi[y, x])
-
-    @property
-    def dst_gbox(self) -> GeoBox:
-        _, y, x = self.idx_tyx
-        return cast(GeoBox, self.gbt[y, x])
-
-
-class _DaskGraphBuilder:
-    # pylint: disable=too-few-public-methods,too-many-instance-attributes
-
-    def __init__(
-        self,
-        cfg: Dict[str, RasterLoadParams],
-        items: List[ParsedItem],
-        tyx_bins: Dict[Tuple[int, int, int], List[int]],
-        gbt: GeoboxTiles,
-        env: Dict[str, Any],
-        rdr: SomeReader,
-        time_chunks: int = 1,
-    ) -> None:
-        self.cfg = cfg
-        self.items = items
-        self.tyx_bins = tyx_bins
-        self.gbt = gbt
-        self.env = env
-        self.rdr = rdr
-        self._tk = tokenize(items, cfg, gbt, tyx_bins, env, time_chunks)
-        self.chunk_shape = (time_chunks, *self.gbt.chunk_shape((0, 0)).yx)
-
-    def __call__(
-        self,
-        shape: Tuple[int, ...],
-        dtype: DTypeLike,
-        /,
-        name: Hashable,
-    ) -> Any:
-        # pylint: disable=too-many-locals
-        assert len(shape) == 3
-        assert isinstance(name, str)
-        cfg = self.cfg[name]
-        assert dtype == cfg.dtype
-
-        chunks = unpack_chunks(self.chunk_shape, shape)
-        tchunk_range = [
-            range(last - n, last) for last, n in zip(np.cumsum(chunks[0]), chunks[0])
-        ]
-
-        cfg_key = f"cfg-{tokenize(cfg)}"
-        gbt_key = f"grid-{tokenize(self.gbt)}"
-
-        dsk: Dict[Hashable, Any] = {
-            cfg_key: cfg,
-            gbt_key: self.gbt,
-        }
-        tk = self._tk
-        band_key = f"{name}-{tk}"
-        md_key = f"md-{name}-{tk}"
-        shape_in_blocks = tuple(len(ch) for ch in chunks)
-
-        for idx, item in enumerate(self.items):
-            band = item.get(name, None)
-            if band is not None:
-                dsk[md_key, idx] = band
-
-        for ti, yi, xi in np.ndindex(shape_in_blocks):  # type: ignore
-            srcs = []
-            for _ti in tchunk_range[ti]:
-                srcs.append(
-                    [
-                        (md_key, idx)
-                        for idx in self.tyx_bins.get((_ti, yi, xi), [])
-                        if (md_key, idx) in dsk
-                    ]
-                )
-
-            dsk[band_key, ti, yi, xi] = (
-                _dask_loader_tyx,
-                srcs,
-                gbt_key,
-                quote((yi, xi)),
-                self.rdr,
-                cfg_key,
-                self.env,
-            )
-
-        return da.Array(dsk, band_key, chunks, dtype=dtype, shape=shape)
 
 
 def _collection(items: Iterable[ParsedItem]) -> RasterCollectionMetadata:
@@ -578,8 +459,7 @@ def load(
     rdr = _reader_driver(load_cfg[bands[0]])
     _rdr_env = rdr.capture_env()
     if chunks is not None:
-        # Dask case: dummy for now
-        _loader = _DaskGraphBuilder(
+        _loader = DaskGraphBuilder(
             load_cfg,
             _parsed,
             tyx_bins,
@@ -588,23 +468,23 @@ def load(
             rdr,
             time_chunks=chunk_shape[0],
         )
-        return _with_debug_info(_mk_dataset(gbox, tss, load_cfg, _loader))
+        return _with_debug_info(mk_dataset(gbox, tss, load_cfg, _loader))
 
-    def _task_stream(bands: List[str]) -> Iterator[_LoadChunkTask]:
+    def _task_stream(bands: List[str]) -> Iterator[LoadChunkTask]:
         _shape: Tuple[int, int, int] = (len(_grouped_idx), *gbt.shape.yx)
         for band_name in bands:
             cfg = load_cfg[band_name]
             for ti, yi, xi in np.ndindex(_shape):  # type: ignore
                 tyx_idx = (ti, yi, xi)
                 srcs = [(idx, band_name) for idx in tyx_bins.get(tyx_idx, [])]
-                yield _LoadChunkTask(band_name, srcs, cfg, gbt, tyx_idx)
+                yield LoadChunkTask(band_name, srcs, cfg, gbt, tyx_idx)
 
-    ds = _mk_dataset(gbox, tss, load_cfg)
+    ds = mk_dataset(gbox, tss, load_cfg)
     ny, nx = gbt.shape.yx
     total_tasks = len(bands) * len(tss) * ny * nx
     _tasks = []
 
-    def _do_one(task: _LoadChunkTask) -> Tuple[str, int, int, int]:
+    def _do_one(task: LoadChunkTask) -> Tuple[str, int, int, int]:
         if debug:
             # print(f"{task.band}[{task.idx_tyx}]")
             _tasks.append(task)
@@ -616,7 +496,7 @@ def load(
             if src is not None
         ]
         with rdr.restore_env(_rdr_env):
-            _ = _fill_2d_slice(srcs, task.dst_gbox, task.cfg, rdr, dst_slice)
+            _ = fill_2d_slice(srcs, task.dst_gbox, task.cfg, rdr, dst_slice)
         t, y, x = task.idx_tyx
         return (task.band, t, y, x)
 
@@ -673,94 +553,6 @@ def _resolve_load_cfg(
         )
 
     return {name: _resolve(name, band) for name, band in bands.items()}
-
-
-def _dask_loader_tyx(
-    srcs: List[List[RasterSource]],
-    gbt: GeoboxTiles,
-    iyx: Tuple[int, int],
-    rdr: SomeReader,
-    cfg: RasterLoadParams,
-    env: Dict[str, Any],
-):
-    assert cfg.dtype is not None
-    gbox = cast(GeoBox, gbt[iyx])
-    chunk = np.empty((len(srcs), *gbox.shape.yx), dtype=cfg.dtype)
-    with rdr.restore_env(env):
-        for i, plane in enumerate(srcs):
-            _fill_2d_slice(plane, gbox, cfg, rdr, chunk[i, :, :])
-        return chunk
-
-
-def _fill_2d_slice(
-    srcs: List[RasterSource],
-    dst_gbox: GeoBox,
-    cfg: RasterLoadParams,
-    rdr: SomeReader,
-    dst: Any,
-) -> Any:
-    # TODO: support masks not just nodata based fusing
-    #
-    # ``nodata``     marks missing pixels, but it might be None (everything is valid)
-    # ``fill_value`` is the initial value to use, it's equal to ``nodata`` when set,
-    #                otherwise defaults to .nan for floats and 0 for integers
-    assert dst.shape == dst_gbox.shape.yx
-    nodata = resolve_src_nodata(cfg.fill_value, cfg)
-
-    if nodata is None:
-        fill_value = float("nan") if dst.dtype.kind == "f" else 0
-    else:
-        fill_value = nodata
-
-    np.copyto(dst, fill_value)
-    if len(srcs) == 0:
-        return dst
-
-    src, *rest = srcs
-    _roi, pix = rdr.read(src, cfg, dst_gbox, dst=dst)
-
-    for src in rest:
-        # first valid pixel takes precedence over others
-        _roi, pix = rdr.read(src, cfg, dst_gbox)
-
-        # nodata mask takes care of nan when working with floats
-        # so you can still get proper mask even when nodata is None
-        # when working with float32 data.
-        missing = nodata_mask(dst[_roi], nodata)
-        np.copyto(dst[_roi], pix, where=missing)
-
-    return dst
-
-
-def _mk_dataset(
-    gbox: GeoBox,
-    time: List[datetime],
-    bands: Dict[str, RasterLoadParams],
-    alloc: Optional[MkArray] = None,
-) -> xr.Dataset:
-    _shape = (len(time), *gbox.shape.yx)
-    coords = xr_coords(gbox)
-    crs_coord_name: Hashable = list(coords)[-1]
-    coords["time"] = xr.DataArray(time, dims=("time",))
-    dims = ("time", *gbox.dimensions)
-
-    def _alloc(shape: Tuple[int, ...], dtype: str, name: Hashable) -> Any:
-        if alloc is not None:
-            return alloc(shape, dtype, name=name)
-        return np.empty(shape, dtype=dtype)
-
-    def _maker(name: Hashable, band: RasterLoadParams) -> xr.DataArray:
-        assert band.dtype is not None
-        data = _alloc(_shape, band.dtype, name=name)
-        attrs = {}
-        if band.fill_value is not None:
-            attrs["nodata"] = band.fill_value
-
-        xx = xr.DataArray(data=data, coords=coords, dims=dims, attrs=attrs)
-        xx.encoding.update(grid_mapping=crs_coord_name)
-        return xx
-
-    return xr.Dataset({name: _maker(name, band) for name, band in bands.items()})
 
 
 def _extract_timestamps(grouped: List[List[ParsedItem]]) -> List[datetime]:
