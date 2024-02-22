e4afea9b79b431f0d453c40021ed7bf4424514a9
Kirill Kouzoubov <kirill888@gmail.com>
2024-02-22 19:18:01 +1100

    Expose more resolve_* methods in odc.loader

--- odc/loader/__init__.py
+++ odc/loader/__init__.py
@@ -4,7 +4,12 @@ Tools for constructing xarray objects from parsed metadata.
 
 from ._builder import chunked_load, resolve_chunk_shape
 from ._driver import reader_driver, register_driver, unregister_driver
-from ._reader import resolve_load_cfg
+from ._reader import (
+    resolve_dst_dtype,
+    resolve_dst_nodata,
+    resolve_load_cfg,
+    resolve_src_nodata,
+)
 from .types import (
     BandIdentifier,
     BandKey,
@@ -34,5 +39,8 @@ __all__ = (
     "unregister_driver",
     "reader_driver",
     "resolve_load_cfg",
+    "resolve_src_nodata",
+    "resolve_dst_nodata",
+    "resolve_dst_dtype",
     "resolve_chunk_shape",
 )
